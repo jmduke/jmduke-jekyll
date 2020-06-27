4e2ceba395c3c885007337efb6b6b418a98da62e
jmduke <me@jmduke.com>
2020-06-27 13:31:43 -0700

    updates

---
+++ _plugins/generator.rb
@@ -0,0 +1,33 @@
+module Jekyll
+    class ContentPageGenerator < Generator
+        safe true
+
+        def generate(site)
+            content = site.data['content']
+            content.each do |item|
+                next if item['date'].nil?
+                site.pages << ContentPage.new(
+                    site,
+                    site.source,
+                    "content/#{Utils.slugify(item['name'])}",
+                    item
+                )
+            end
+        end
+    end
+
+    class ContentPage < Page
+        def initialize(site, base, dir, item)
+            @site = site
+            @base = base
+            @dir  = dir
+            @name = 'index.html'
+
+            self.process(@name)
+            self.read_yaml(File.join(base, '_layouts'), 'content.html')
+            self.data['item'] = item
+            self.data['summary'] = item['summary']
+            self.data['title'] = item['name']
+        end
+    end
+end
