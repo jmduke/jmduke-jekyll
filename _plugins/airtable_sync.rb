23a803cb3e7a45569e108882769129924b1c8779
jmduke <me@jmduke.com>
2020-08-05 12:21:17 -0700

    Do the thing

--- _plugins/airtable_sync.rb
+++ _plugins/airtable_sync.rb
@@ -1,44 +1,46 @@
+# frozen_string_literal: true
+
 require 'json'
 require 'airtable'
 require 'active_support/all'
 
 # Pass in api key to client
-@client = Airtable::Client.new("keyX4yPY1qYqq1pad")
+AIRTABLE_API_KEY = 'keyX4yPY1qYqq1pad'
+AIRTABLE_TABLE_ID = 'app5RDJQQni8Itd2D'
+
+@client = Airtable::Client.new(AIRTABLE_API_KEY)
 
 def paginate_indefinitely(table)
-    all_results = []
-    offset = nil
-    while true
-        page = table.records(:view => 'Grid view', :offset => offset)
-        all_results += page.map(&:attributes)
-        offset = page.offset
-        break if offset.nil?
-    end
-    all_results
+  all_results = []
+  offset = nil
+  loop do
+    page = table.records(view: 'Grid view', offset: offset)
+    all_results += page.map(&:attributes)
+    offset = page.offset
+    break if offset.nil?
+  end
+  all_results
 end
 
+def collect_records(sheet_name)
+  paginate_indefinitely(@client.table(AIRTABLE_TABLE_ID, sheet_name))
+end
 
-# Pass in the app key and table name
-content_records = paginate_indefinitely(@client.table("app5RDJQQni8Itd2D", "Content"))
+def write_records(records, output_path)
+  File.open(output_path, 'w') do |f|
+    f.write(records.to_json)
+  end
+end
+
+content_records = collect_records('Content')
+notebook_records = collect_records('Notebook')
 content_id_to_data = Hash[content_records.collect { |v| [v['id'], v] }]
+notebook_records.each do |record|
+  next unless record['source']
 
-notebook_records = paginate_indefinitely(@client.table("app5RDJQQni8Itd2D", "Notebook"))
-
-notebook_records.each { |record|
-    next unless record["source"]
-    record["source"] = content_id_to_data[record["source"][0]]
-}
-# Change the filename here below but make sure it's in the _data folder.
-File.open("_data/notebook.json", "w") do |f|
-    f.write(notebook_records.to_json)
-end
-File.open("_data/content.json", "w") do |f|
-    f.write(content_records.to_json)
+  record['source'] = content_id_to_data[record['source'][0]]
 end
 
-
-words_records = paginate_indefinitely(@client.table("app5RDJQQni8Itd2D", "Dictionary"))
-
-File.open("_data/words.json", "w") do |f|
-    f.write(words_records.to_json)
-end
+write_records(notebook_records, '_data/notebook.json')
+write_records(content_records, '_data/content.json')
+write_records(collect_records('Dictionary'), '_data/words.json')
---
+++ interviews.md
@@ -0,0 +1,60 @@
+---
+layout: page
+title: Press
+---
+
+I have been fortunate enough to get to navelgaze, opine, and/or thought-lead on a number of podcasts:
+
+<table>
+<tr>
+<td>2020</td>
+<td><a href="https://newslettercrew.com/podcast/episode-3-monetization-niche-topics-and-publishing-frequency-with-justin-duke-of-buttondown/">Newsletter Crew</a></td>
+</tr>
+<tr>
+<td>2020</td>
+<td><a href="https://open.spotify.com/episode/4KNSqBUu9bM2WjS23yhoNN">Level Up</a></td>
+</tr>
+<tr>
+<td>2020</td>
+<td><a href="https://anchor.fm/mintmakers/episodes/Episode-1---Talking-to-the-founder-of-Buttondown-and-Spoonbill--Justin-Duke-eck5nd">MintMakers</a></td>
+</tr>
+<tr>
+<td>2019</td>
+<td><a href="https://podcast.productivityintech.com/episodes/its-easy-when-youre-customer-number-one">Productivity in Tech</a></td>
+</tr>
+</table>
+
+<br />
+Similarly, sometimes I or my projects are covered in the press. I find this less consequential but slightly more fun.
+<br />
+
+<table>
+<tr>
+<td>2019</td>
+<td><a href="https://www.theatlantic.com/technology/archive/2019/10/substack-revue-email-newsletter-startups-tinyletter/599557/">The Atlantic</a></td>
+</tr>
+<tr>
+<td>2018</td>
+<td><a href="https://www.theverge.com/2018/1/5/16855304/mailchimp-tinyletter-integration-platform-changes">Vox</a></td>
+</tr>
+<tr>
+<td>2018</td>
+<td><a href="https://www.vox.com/2018/1/28/16941702/spoonbill-twitter-bio-location-change-emails-bioischanged">Vox (again)</a></td>
+</tr>
+<tr>
+<td>2015</td>
+<td><a href="https://gizmodo.com/if-you-re-looking-for-some-saturday-morning-inspiration-1737098561">Gizmodo</a></td>
+</tr>
+</table>
+
+<style>
+table {
+    width: 100%;
+}
+td:first-child {
+    opacity: 0.7;
+}
+td:last-child {
+    text-align: right;
+}
+</style>
